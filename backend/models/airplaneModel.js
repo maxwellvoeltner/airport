871405b0b80764fdceca6e8263a14b3049856a2b
maxwellvoeltner <147751908+maxwellvoeltner@users.noreply.github.com>
2026-01-17 12:51:30 -0500

    Refactor airplane model structure and export

--- backend/models/airplaneModel.js
+++ backend/models/airplaneModel.js
@@ -1,17 +1,8 @@
 // getting mongoose
 const mongoose = require('mongoose')
 
-/*
-It defines the structure of documents within a MongoDB collection
-A schema in Mongoose maps directly to a MongoDB collection
-and defines the shape of the documents within that collection.
-*/
 const Schema = mongoose.Schema
 
-/*
-creating structure for all airplane "documents" in the MongoDB
-first argument: schema object
-*/
 const airplaneSchema = new Schema({
 
      // acronym of airport airplane is arriving from and departing to
@@ -65,4 +56,5 @@ const airplaneSchema = new Schema({
 })
 
 // exporting the airplane model
+
 module.exports = mongoose.model('Airplane', airplaneSchema, 'cluster0')
